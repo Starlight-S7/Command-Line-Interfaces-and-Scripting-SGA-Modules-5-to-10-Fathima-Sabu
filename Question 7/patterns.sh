1a54000c75c2253f4c625d1de35edbb14c187e21
Starlight S <starsong23605@gmail.com>
2026-02-05 17:25:06 +0530

    Update patterns.sh

--- Question 7/patterns.sh
+++ Question 7/patterns.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+# Extract words, convert to lowercase
+words=$(tr -cs 'a-zA-Z' '\n' < "$1" | tr 'A-Z' 'a-z')
+
+# Only vowels
+echo "$words" | grep -E '^[aeiou]+$' > vowels.txt
+
+# Only consonants
+echo "$words" | grep -E '^[^aeiou]+$' > consonants.txt
+
+# Mixed words starting with a consonant
+echo "$words" | grep -E '^[^aeiou][a-z]*[aeiou][a-z]*$' > mixed.txt
